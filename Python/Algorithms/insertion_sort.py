30494dfbee3d7c6936bf6ea26eded280e005b8d8
Yahaya Usman <yahus591@student.liu.se>
2021-11-28 01:57:31 +0100

    added sorting in descending order

--- Python/Algorithms/insertion_sort.py
+++ Python/Algorithms/insertion_sort.py
@@ -6,7 +6,7 @@ def insertion_sort(A):
     for i in range(1,len(A)):
         j = i - 1
         key = A[i]
-        while j >= 0 and A[j] > key:
+        while j >= 0 and A[j] > key: # A[j] < key to sort in descending order
             # swap
             A[j+1] = A[j]
             j = j - 1
